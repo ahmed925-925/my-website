b8a848c30ceb3342c964f9b533709688722e60c8
Ahmed Mohamed <118309402+amedmohmed925@users.noreply.github.com>
2025-10-11 01:31:34 +0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -75,7 +75,7 @@ export default function Hero() {
               </motion.a>
 
               <motion.a
-                href="https://drive.google.com/file/d/1ACv2p-k_wbD-OLI6ove3bjpXm0FkBDIp/view?usp=sharing"
+                href="https://drive.google.com/file/d/1O2jlPEzHq7K4XzUIAH51j-RxbgtGXKCG/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.05 }}
